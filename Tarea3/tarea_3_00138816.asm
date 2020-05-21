a91106021fbfd8a564012d78ed1db891fd1904a9
Raul Granados <00138816@uca.edu.sv>
2020-05-20 22:15:35 -0600

    agregando al segunda frase

--- Tarea3/tarea_3_00138816.asm
+++ Tarea3/tarea_3_00138816.asm
@@ -43,9 +43,9 @@ m_cursr:mov 	ah, 02h
 	int 	10h
 	ret
 
-phrase:	mov 	di, 3d
+phrase:	mov 	di, 16d
 
-lupi:	mov 	cl, [msg+di-3d]
+lupi:	mov 	cl, [msg+di-16d]
 	call    m_cursr
 	call 	w_char
 	inc	di
@@ -53,8 +53,29 @@ lupi:	mov 	cl, [msg+di-3d]
 	jb	lupi
 	ret
 
+;2
+
+m_cursr2:mov 	ah, 02h
+	mov 	dx, di  ; columna
+	mov 	dh, 9d ; fila
+	mov 	bh, 0h
+	int 	10h
+	ret
+
+phrase2:	mov 	di, 35d
+lupi2:	mov 	cl, [msg2+di-35d]
+	call    m_cursr2
+	call 	w_char
+	inc	di
+	cmp 	di, len2
+	jb	lupi2
+	ret
+
 
 section .data
-msg	db 	"Stuff the taco"
-len 	equ	$-msg+3d
+msg	    db 	"Stuff the taco"
+len 	equ	$-msg+16d
+
+msg2	db 	"Butter the biscuit"
+len2 	equ	$-msg2+35d
 
